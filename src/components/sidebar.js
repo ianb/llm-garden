cda0a902c4c2df0db994a42549827855be4965b2
Ian Bicking <ian@ianbicking.org>
2022-12-18 09:32:43 -0600

    Remove double absolute from sidebar
    But this also breaks cyoa. Not sure why it was in there or how it all got so complicated. Will need to revisit

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,6 +1,7 @@
 const Sidebar = ({ children }) => {
+  // FIXME: can't decide if "absolute" is helpful or harmful
   return (
-    <div class="absolute">
+    <div class="x-absolute">
       <div class="absolute sticky top-0 right-0 float-right w-1/3 rounded-l drop-shadow-l ml-2 bg-white p-2 pl-3">
         {children}
       </div>
