05fac7e70750af781813977884e8fecfd10fc333
Ian Bicking <ian@ianbicking.org>
2023-02-15 17:00:04 -0600

    Guard the GPT key checker

--- src/key-management/key.js
+++ src/key-management/key.js
@@ -3,6 +3,8 @@ import KeyHolder from "./keyholder";
 const standardKey =
   "FFB0le+/wQs711zpoAgN/mKBvc01vOD4r8OiZaNHzvWSSroS0n/p8onJmWSRD+SKb8cw6SahQ6XdpwiRcKCOFROAlNfLIv1AJFO2XIN5TQ==";
 
-export const holder = new KeyHolder("gpt3", [standardKey], (v) =>
-  v.startsWith("sk-")
+export const holder = new KeyHolder(
+  "gpt3",
+  [standardKey],
+  (v) => v && v.startsWith("sk-")
 );
