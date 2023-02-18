44664d2c3d980f09fea60f1b20d4eb3daf30c1ca
Ian Bicking <ian@ianbicking.org>
2023-02-18 13:29:51 -0600

    Include a description of peoplesim

--- src/myoa/index.js
+++ src/myoa/index.js
@@ -1,6 +1,7 @@
 /* eslint no-unused-vars: "off" */
 import { StoryView } from "./storyview";
 import { ModelIndexPage } from "../components/modelindex";
+import { InfoHeader, P } from "../components/common";
 import storyDb from "./storydb";
 
 export const AdventureIndex = () => {
@@ -9,6 +10,19 @@ export const AdventureIndex = () => {
       title="Make Your Own Adventure"
       store={storyDb}
       viewer={StoryView}
-    />
+    >
+      <InfoHeader title="Make your own Choose Your Own Adventure-style story">
+        <P>
+          This is a tool to build your own adventures using GPT. You'll setup
+          the style of the story and GPT will create passages and possible
+          choices. <em>You</em> select the specific choices, and control the
+          generation through a mini chat-like interface built into the editing.
+        </P>
+        <P>
+          There's also Dall-E integration to create illustrations for individual
+          passages.
+        </P>
+      </InfoHeader>
+    </ModelIndexPage>
   );
 };
--- src/peoplesim/index.js
+++ src/peoplesim/index.js
@@ -1,10 +1,24 @@
 /* eslint no-unused-vars: "off" */
 import { PeopleView } from "./peopleview";
 import { ModelIndexPage } from "../components/modelindex";
+import { InfoHeader, P } from "../components/common";
 import { peopleDb } from "./peopledb";
 
 export const PeopleSimIndex = () => {
   return (
-    <ModelIndexPage title="People Sim" store={peopleDb} viewer={PeopleView} />
+    <ModelIndexPage title="People Sim" store={peopleDb} viewer={PeopleView}>
+      <InfoHeader title="Multi-person scene simulation">
+        <P>
+          This explores having multiple distinct entities (people) with their
+          own moods and goals interacting in an environment.
+        </P>
+        <P>
+          You, the user, setup each scene; the characters, their initial goals
+          and moods, their relationships with each other, and the environment in
+          which they are acting. Then you can step forward in time and GPT will
+          generate the next action for each character.
+        </P>
+      </InfoHeader>
+    </ModelIndexPage>
   );
 };
