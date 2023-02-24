2a9a3b4bde614e420982435f7a08215be1104de3
Ian Bicking <ian@ianbicking.org>
2023-02-23 19:23:42 -0600

    Try to use an enterkeyhint attribute on submittable fields

--- src/components/common.js
+++ src/components/common.js
@@ -188,6 +188,7 @@ export const TextInput = (props) => {
       class:
         "shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline",
       onKeyDown,
+      enterKeyHint: props.onSubmit ? "enter" : null,
     },
     props
   );
