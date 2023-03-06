36e1715c8ed3ef22e82fab065812c6c8beb9d4ab
Ian Bicking <ian@ianbicking.org>
2023-03-05 19:05:39 -0600

    Use InfoA link

--- src/imagegen/index.js
+++ src/imagegen/index.js
@@ -2,7 +2,7 @@
 import { ImageGenView } from "./imagegenview";
 import { ModelIndexPage } from "../components/modelindex";
 import { imageGenDb } from "./imagegendb";
-import { InfoHeader, P, A } from "../components/common";
+import { InfoHeader, P, A, InfoA } from "../components/common";
 
 export const ImageGenIndex = () => {
   return (
@@ -14,11 +14,7 @@ export const ImageGenIndex = () => {
       <InfoHeader title="Image Generator">
         <P>
           This is just a testbed for doing image generation, such as what is
-          used in{" "}
-          <A class="text-blue-200 hover:text-blue-300" href="/myoa">
-            Make Your Own Adventure
-          </A>
-          .
+          used in <InfoA href="/myoa">Make Your Own Adventure</InfoA>.
         </P>
         <P>
           There are no particularly interesting featuers in this, it just
