8ba3bda9f99b2f63c37d0a85311f86ba0287fe63
Ian Bicking <ianb@colorstudy.com>
2023-04-04 09:56:34 -0500

    Add links to blog post and video

--- src/layercraft/index.js
+++ src/layercraft/index.js
@@ -18,6 +18,10 @@ export const LayerCraftIndex = () => {
         <P>
           This is a generalization of <InfoA href="/citymaker">City Maker</InfoA>. That was an app built around a certain idea, but this separates the idea from the app; all the prompts and structure and dependencies are defined separately.
         </P>
+        <P>
+          Read more about this tool in <InfoA href="https://ianbicking.org/blog/2023/04/world-building-gpt-2-declarative.html">World Building with GPT part 2: bigger, better, more declarative</InfoA>
+        </P>
+        <YouTube videoId="4DEibveZ-n8" />
       </InfoHeader>
     </ModelIndexPage>
   );
